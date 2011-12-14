72f9828cc9eaf2b6a1f124d7d60e6f9a2e8a17d3
martines.marco@gmail.com <martines.marco@gmail.com@1a1dd34d-b84e-62aa-d0d8-f409c07d3150>
2011-12-14 07:07:17 +0000

    Modified Client example

--- branches/CompatibleVerIDE10/example/GSM_GPRSLibrary_Client/GSM_GPRSLibrary_Client.ino
+++ branches/CompatibleVerIDE10/example/GSM_GPRSLibrary_Client/GSM_GPRSLibrary_Client.ino
@@ -36,6 +36,12 @@ void setup()
     Serial.println("status=ATTACHED");
   else Serial.println("status=ERROR");
   delay(10000);
+
+  //Read IP address.
+  gsm.writeB("AT+CIFSR\r");
+  gsm.read(msg, 200);
+  Serial.println(msg);
+  delay(5000); 
   
   //TCP Client GET, send a GET request to the server and
   //save the reply.
