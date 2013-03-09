83974a2411aac2e4e899e1b1ec356568c2de7a56
martines.marco <martines.marco@1a1dd34d-b84e-62aa-d0d8-f409c07d3150>
2013-03-09 16:44:35 +0000

    Fixed AT example, erroneously changed in Client example

--- examples/GSM_GPRSLibrary_AT/GSM_GPRSLibrary_AT.ino
+++ examples/GSM_GPRSLibrary_AT/GSM_GPRSLibrary_AT.ino
@@ -1,6 +1,6 @@
 #include "SIM900.h"
 #include <SoftwareSerial.h>
-#include "inetGSM.h"
+//#include "inetGSM.h"
 //#include "sms.h"
 //#include "call.h"
 
@@ -10,17 +10,16 @@
 //www.open-electronics.org
 //this code is based on the example of Arduino Labs.
 
-//Simple sketch to start a connection as client.
+//Simple sketch to communicate with SIM900 through AT commands.
 
-InetGSM inet;
+//InetGSM inet;
 //CallGSM call;
 //SMSGSM sms;
 
-char msg[50];
 int numdata;
-char inSerial[50];
+char inSerial[40];
 int i=0;
-boolean started=false;
+
 
 void setup() 
 {
@@ -29,35 +28,9 @@ void setup()
   Serial.println("GSM Shield testing.");
   //Start configuration of shield with baudrate.
   //For http uses is raccomanded to use 4800 or slower.
-  if (gsm.begin(4800)){
+  if (gsm.begin(9600))
     Serial.println("\nstatus=READY");
-    started=true;  
-    gsm.forceON();
-  }
   else Serial.println("\nstatus=IDLE");
-  if(started){
-    //GPRS attach, put in order APN, username and password.
-    //If no needed auth let them blank.
-    if (inet.attachGPRS("internet.wind", "", ""))
-      Serial.println("status=ATTACHED");
-    else Serial.println("status=ERROR");
-    delay(1000);
-    
-    //Read IP address.
-    gsm.SimpleWriteln("AT+CIFSR");
-    delay(5000);
-    //Read until serial buffer is empty.
-    gsm.WhileSimpleRead();
-  
-    //TCP Client GET, send a GET request to the server and
-    //save the reply.
-    numdata=inet.httpGET("www.google.com", 80, "/", msg, 50);
-    //Print the results.
-    Serial.println("\nNumber of data received:");
-    Serial.println(numdata);  
-    Serial.println("\nData received:"); 
-    Serial.println(msg); 
-  }
 };
 
 void loop() 
@@ -89,11 +62,7 @@ void serialhwread(){
     if(!strcmp(inSerial,"TEST")){
       Serial.println("SIGNAL QUALITY");
       gsm.SimpleWriteln("AT+CSQ");
-    }
-    //Read last message saved.
-    if(!strcmp(inSerial,"MSG")){
-      Serial.println(msg);
-    }
+    } 
     else{
       Serial.println(inSerial);
       gsm.SimpleWriteln(inSerial);
